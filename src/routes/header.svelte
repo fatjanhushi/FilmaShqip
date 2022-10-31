c828ea1d551383403fc9f2b483339a88250eedd2
Fatjon Hushi <me@fatjoni.com>
2022-10-31 18:56:59 +0100

    Commit some styles to header

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -7,18 +7,70 @@
 			height="90px"
 		/>
 	</figure>
-	<input type="search" />
+
+	<figure>
+		<img
+			src="https://www.getautismactive.com/wp-content/uploads/2021/01/Test-Logo-Circle-black-transparent.png"
+			alt="Logo"
+			width="90px"
+			height="90px"
+		/>
+	</figure>
 </header>
-<nav class="container">test</nav>
+<nav class="container">
+	<ul>
+		<li>
+			<a href="#">Menu</a>
+		</li>
+		<li>
+			<a href="#">Watchlist</a>
+		</li>
+	</ul>
+
+	<input type="search" placeholder="Search" />
+
+	<span>Movies</span><span>&nbsp|&nbsp</span><span>TV</span>
+</nav>
 
 <style lang="scss">
 	header {
 		display: flex;
 		align-items: center;
+		justify-content: space-between;
+		margin-top: 2rem;
+		background-color: white;
+		border-top-left-radius: 1rem;
+		border-top-right-radius: 1rem;
+	}
 
+	nav {
+		background-color: #232323;
+		display: flex;
+		color: white;
+		ul {
+			display: flex;
+			align-items: center;
+			justify-content: space-around;
+			li {
+				padding: 1rem 0.5rem;
+				a {
+					color: inherit;
+				}
+			}
+		}
 		input {
-			height: 2rem;
-			padding: 1rem;
+			background-color: transparent;
+			width: 100%;
+			color: inherit;
+			&[type='search'] {
+				height: 3rem;
+				border-radius: 1rem;
+				margin: auto 1rem;
+				padding-inline: 0.5rem;
+			}
+		}
+		span {
+			margin: auto 0;
 		}
 	}
 </style>
